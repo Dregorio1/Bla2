7782807f506756d87d48e7cf78f0f776810828a1
Dregorio1 <71985654+Dregorio1@@users.noreply.github.com>
2021-05-20 17:45:53 +0200

    first

---
+++ app/src/main/java/com/example/myapplication/FragmentTop.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class FragmentTop extends Fragment {
+
+    public WebView mWebView;
+
+    public FragmentTop() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.fragment_top, container, false);
+        mWebView = v.findViewById(R.id.webview_top);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://google.com");
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
